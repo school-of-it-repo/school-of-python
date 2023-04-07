983fbec0614660c6a533014ecd83d1fae59509e3
School of IT <mail2schoolofit@gmail.com>
2023-04-07 18:01:07 +0530

    Grouping elements

--- schoolofit/workout/2_grouping_the_element.py
+++ schoolofit/workout/2_grouping_the_element.py
@@ -1,3 +1,23 @@
+'''
+
+Input = [
+        'apple'
+        , 'orange'
+        , 'apple'
+        , 'bannana'
+        , 'gauva'
+        , 'apple'
+        , 'orange'
+        , 'apple'
+        , 'bannana'
+        , 'apple'
+        , 'orange'
+    ]
+Output : [{'apple': 5}, {'bannana': 2}, {'gauva': 1}, {'orange': 3}]
+
+
+'''
+
 import itertools
 if __name__ == '__main__':
     fruits = [
